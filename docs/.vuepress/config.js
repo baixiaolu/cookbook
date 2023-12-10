566e892206e079cb9eb839621db54119b1370397
baixiaolu <baixiaolu2005@qq.com>
2023-12-10 20:31:40 +0800

    时隔一周，再次尝试

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     title: '白大白的开箱即用手册',  // 设置网站标题
     dest: './docs/.vitepress/dist',    // 设置输出目录
-    base: '/cookbook/', // 设置站点根路径
-    repo: 'https://github.com/baixiaolu/cookbook' // 添加 github 链接
+    base: '/', // 设置站点根路径
+    repo: 'https://github.com/baixiaolu' // 添加 github 链接
 }
