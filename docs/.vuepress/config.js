7eb978f6b937fe0fced8386c56f1283898aa487e
baixiaolu <baixiaolu2005@qq.com>
2023-12-10 21:11:57 +0800

    修改仓库地址

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,5 @@ module.exports = {
     title: '白大白的开箱即用手册',  // 设置网站标题
     dest: './docs/.vuepress/dist',    // 设置输出目录
     base: '/', // 设置站点根路径
-    repo: 'https://github.com/baixiaolu' // 添加 github 链接
+    repo: 'https://github.com/baixiaolu/init' // 添加 github 链接
 }
