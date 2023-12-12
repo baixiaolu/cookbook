53310a31eda2681299d41221fcf6c153e6e70c21
baixiaolu <baixiaolu2005@qq.com>
2023-12-12 19:43:02 +0800

    尝试开启顶栏和侧边栏，未成功

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,5 +2,19 @@ module.exports = {
     title: '白大白的开箱即用手册',  // 设置网站标题
     dest: './docs/.vuepress/dist',    // 设置输出目录
     base: '/', // 设置站点根路径
-    repo: 'https://github.com/baixiaolu/init' // 添加 github 链接
+    repo: 'https://github.com/baixiaolu/init', // 添加 github 链接
+    themeConfig: {
+        search: false,
+        searchMaxSuggestions: 5,
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: [
+            '/',
+            '/page-a',
+            ['/page-b', 'Explicit link text']
+        ]
+    }
 }
